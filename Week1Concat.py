5839d82c36940591db490420e04265687bb1d942
VincentLiao777 <junyu_liao@berkeley.edu>
2026-06-29 09:52:43 +0800

    Week1Concat - 06292026

---
+++ Week1Concat.py
@@ -0,0 +1,35 @@
+
+import numpy as np
+import pandas as pd
+import math
+
+string1 = "Hello"
+string2 = "World"
+string3 = string1 + ' ' + string2
+print(string3)
+
+monthList = ['202401','202402','202403','202404','202405','202406','202407','202408',
+             '202409','202410','202411','202412','202501','202502','202503','202504',
+             '202505','202506','202507','202508','202509','202510','202511','202512',
+             '202601','202602','202603','202604']
+soldName = 'CRMLSSold'
+listingName = 'CRMLSListing'
+location = r'D:\Data&Documents\Berkeley\Internship\IDX Exchange\IDX_raw_data\\'
+
+print(location + soldName + monthList[0])
+
+sold1 = pd.read_csv(location + soldName + monthList[0] + '.csv', encoding='latin-1', low_memory=False)
+sold2 = pd.read_csv(location + soldName + monthList[1] + '.csv', encoding='latin-1', low_memory=False)
+sold = pd.concat([sold1, sold2])
+list1 = pd.read_csv(location + listingName + monthList[0] + '.csv', encoding='latin-1', low_memory=False)
+list2 = pd.read_csv(location + listingName + monthList[1] + '.csv', encoding='latin-1', low_memory=False)
+listing = pd.concat([list1, list2])
+
+for i in range(2,len(monthList)):
+    newsold = pd.read_csv(location + soldName + monthList[i] + '.csv', encoding='latin-1', low_memory=False)
+    sold = pd.concat([sold, newsold])
+    newlist = pd.read_csv(location + listingName + monthList[i] + '.csv', encoding='latin-1', low_memory=False)
+    listing = pd.concat([listing, newlist])
+
+sold.to_csv('Sold_concated.csv',index = False, encoding='latin-1')
+listing.to_csv('Listing_concated.csv', index = False, encoding='latin-1')
